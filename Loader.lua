8ee53ea1d685e6f2e95afa4caaff20623f1a6b42
Zeuxtronic <zeuxtronic@gmail.com>
2025-07-02 12:55:46 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-getgenv().T = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeuxtronic/Final-Zeouron/refs/heads/main/Libraries/ZeoTool.lua"))()
+getgenv().T = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeuxtronic/Zeouron/refs/heads/main/Libraries/ZeoTool.lua"))()
 
 T.ConstructFolder()
 if not getgenv().ZeouronExecuted or readfile("Zeouron/Settings/Developer.txt") == "true" then
